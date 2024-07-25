91cdfc4d0a94c50a4da2908464369b8645d6fa95
Fomenko_Viacheslav <mrmagosj@gmail.com>
2024-07-25 17:17:49 +0300

    add animation

--- js/burger.js
+++ js/burger.js
@@ -1,13 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-	const burgerIcon = document.getElementById("burger-icon");
-	const burgerMenu = document.getElementById("burger-menu");
-	const closeBtn = document.getElementById('close-btn')
-	burgerIcon.addEventListener("click", function() {
-			burgerMenu.classList.toggle("burger-menu-active");
-			burgerIcon.classList.toggle("active");
-	});
-	closeBtn.addEventListener("click", function() {
-		burgerMenu.classList.toggle("burger-menu-active");
-		burgerIcon.classList.toggle("active");
-	});
+  const burgerIcon = document.getElementById("burger-icon");
+  const burgerMenu = document.getElementById("burger-menu");
+  const closeBtn = document.getElementById('close-btn');
+
+  burgerIcon.addEventListener("click", function() {
+    burgerMenu.classList.add("burger-menu-active");
+    burgerIcon.classList.add("active");
+  });
+
+  closeBtn.addEventListener("click", function() {
+    burgerMenu.classList.remove("burger-menu-active");
+    burgerMenu.classList.add("burger-menu-closing");
+    burgerIcon.classList.remove("active");
+
+    setTimeout(function() {
+      burgerMenu.classList.remove("burger-menu-closing");
+    }, 500);
+  });
 });
